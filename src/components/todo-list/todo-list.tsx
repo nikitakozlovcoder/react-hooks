6fe9cd691186be7288d4a98fc1e169faabf3b93a
Nikita Kozlov <n.kozlov@inostudio.ru>
2023-07-23 12:27:03 +0300

    search input & hooks

--- src/components/todo-list/todo-list.tsx
+++ src/components/todo-list/todo-list.tsx
@@ -1,39 +1,29 @@
-﻿import React, {useEffect, useState} from "react";
+﻿import React from "react";
 import {ToDoModel} from "../../models/to-do.model";
-import axios from "axios";
-import {TodosPaginatedResponseModel} from "../../models/todos-paginated-response.model";
 import {Todo} from "../todo/todo";
 import styles from './todo-list.module.scss';
+import {useHttp} from "../../utils/hooks/UseHttp";
+import {TodosPaginatedResponseModel} from "../../models/todos-paginated-response.model";
 
 export const TodoList = () => {
-  const [todos, setToDos] = useState<ToDoModel[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-  useEffect(() => {
-    const cancelToken = axios.CancelToken.source();
-    setIsLoading(true);
-    axios.get<TodosPaginatedResponseModel>('https://dummyjson.com/todos', {cancelToken: cancelToken.token})
-      .then(x => {
-        setToDos(x.data.todos);
-        setIsLoading(false);
-      });
-    
-    return () => {
-      cancelToken.cancel();
-    }
+  const [todosResponse, todosErr, todosState] = useHttp<TodosPaginatedResponseModel>({
+    url:'https://dummyjson.com/todos'
   }, []);
+  
 
   const buildList = ()=> {
-    return <>{todos.map(x => {
-      return <div className={styles.card}>
+    return todosResponse!.todos.map(x => {
+      return <div className={styles.card} key={x.id}>
         <Todo todo={x}/>
       </div>
-    })}</>
+    });
   }
 
   return (
     <>
-      {isLoading && <div>Loading</div>}
-      {!isLoading && buildList()}
+      {todosState === 'loading' && <div>Loading</div>}
+      {todosState === 'error' && <div>{todosErr?.message}</div>}
+      {todosState === 'ok' && buildList()}
     </>
   );
 }
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,13 @@
 ﻿import {TodoList} from "../../components/todo-list/todo-list";
+import {Search} from "../../components/search/Search";
+import {useState} from "react";
 
 export const Home = () => {
+  const [search, setSearch] =  useState('');
   return (
     <>
       <h1>ToDos</h1>
+      <Search searchString={search} onSearchChange={(str) => setSearch(str)}/>
       <hr/>
       <TodoList/>
     </>
